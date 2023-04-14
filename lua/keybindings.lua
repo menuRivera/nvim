d7e8fbabeb405b52229ae8194afca902f999be68
Manuel Rivera <menu.rivera@gmail.com>
2023-04-13 21:42:07 -0600

    [MOD] -> keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -12,7 +12,6 @@ end
 
 
 -- nvim
--- map('i', 'jk', '<ESC>')
 map('n', '<leader>w', '<Cmd>w<CR>')
 map('n', '<CR>', 'i<CR><ESC>')
 map('i', '{', '{}<left>')
@@ -25,10 +24,13 @@ map('n', '<C-h>', '<Cmd>NvimTreeToggle<CR>')
 
 -- nvim-comment
 map({'i', 'n'}, '<C-/>', '<Cmd>CommentToggle<CR>')
--- fix
+-- fix: multiple lines comment toggle
 map('x', '<C-/>', "<Cmd>'<,'>CommentToggle<CR>")
 
 -- bufferline
+-- next buffer
 map('n', 'gt', '<Cmd>bn<CR>')
+-- previous buffer
 map('n', 'gT', '<Cmd>bp<CR>')
+-- close buffer
 map('n', '<C-w>', '<Cmd>bd<CR>')
