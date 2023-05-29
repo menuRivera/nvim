a6b94cf869e20c4a8b6eb59d0909fb9e3f5c5162
Manuel Rivera <menu.rivera@gmail.com>
2023-05-29 16:24:19 -0600

    [FIX] -> toggleterm keybinding

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,5 +1,13 @@
 require'toggleterm'.setup{
-	shade_terminals = true	
+	shade_terminals = true,
+	open_mapping = [[<c-`>]],
+	hide_numbers = true,
+	direction = 'float',
+	autochdir = true,
+	float_opts = {
+		border = 'curved',
+		winblend = 8
+	},
 }
 
 -- local Terminal = require'toggleterm.terminal'.Terminal
