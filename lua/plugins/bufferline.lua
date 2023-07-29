52a53b33b794d4b1050755a8d156f6b049d80c5d
Manuel Rivera <menu.rivera@gmail.com>
2023-07-28 19:32:43 -0600

    [ADD] -> styles modifications on bufferline and lualine

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,7 +1,14 @@
 require('bufferline').setup{
 	options = {
+		separator_style = 'slope',
+		-- separator_style = 'slant',
 		show_buffer_icons = false,
 		always_show_bufferline = false,
+		hover = {
+                enabled = true,
+                delay = 200,
+                reveal = {'close'}
+            },
 		offsets = {
 			{
 				filetype = "NvimTree",
