6336d236df0b7ff154f08f4e72295a156339920c
Manuel Rivera <menu.rivera@gmail.com>
2023-12-13 15:20:29 -0600

    [ADD|MOD] -> added catppuccin (not implemented), modded lualine a little

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,14 +1,15 @@
-require('bufferline').setup{
+require('bufferline').setup {
+	-- highlights = require("catppuccin.groups.integrations.bufferline").get(),
 	options = {
 		separator_style = 'slope',
 		-- separator_style = 'slant',
 		show_buffer_icons = false,
 		always_show_bufferline = false,
 		hover = {
-                enabled = true,
-                delay = 200,
-                reveal = {'close'}
-            },
+			enabled = true,
+			delay = 200,
+			reveal = { 'close' }
+		},
 		offsets = {
 			{
 				filetype = "NvimTree",
