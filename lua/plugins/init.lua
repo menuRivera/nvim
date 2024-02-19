150fdf06a394227187e0df11cba49d39e3a79bbf
Manuel Rivera <menu.rivera@gmail.com>
2024-02-18 18:31:13 -0600

    [MOD] -> initial lazy.nvim setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,100 +1,47 @@
-require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	-- theme
-	use 'folke/tokyonight.nvim'
-	-- use 'nvim-tree/nvim-web-devicons'
-	use {
-		"projekt0n/circles.nvim",
-		requires = { "nvim-tree/nvim-web-devicons" }
-	}
-
-	-- markdown previewer
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = function() vim.fn["mkdp#util#install"]() end,
+-- handle lazyvim installation
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
 	})
+end
+vim.opt.rtp:prepend(lazypath)
 
-	-- lualine
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
-	-- nvim-tree
-	use {
-		'nvim-tree/nvim-tree.lua',
-		requires = {
-			'nvim-tree/nvim-web-devicons', -- optional, for file icons
+-- plugins
+require('lazy').setup({
+	{ 'folke/tokyonight.nvim',     lazy = false },
+	-- install without yarn or npm
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function() vim.fn["mkdp#util#install"]() end,
+	},
+	{ 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
 		},
-		tag = 'nightly'           -- optional, updated every week. (see issue #1193)
-	}
-
-	-- bufferline
-	use { 'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons' }
-	-- telescope?
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.4',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
-
-	-- Terminal
-	use { "akinsho/toggleterm.nvim", tag = '*' }
-
-	-- Trouble
-	use {
-		"folke/trouble.nvim",
-		requires = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("trouble").setup {
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			}
-		end
-	}
-
-	-- gitsigns
-	use 'lewis6991/gitsigns.nvim'
-
-	-- use("petertriho/nvim-scrollbar")
-	use 'nvim-treesitter/nvim-treesitter'
-
-	-- Comments
-	use "numToStr/Comment.nvim"
-	use 'JoosepAlviste/nvim-ts-context-commentstring'
-
-	-- LSP
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
-		requires = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' },
-			{ 'williamboman/mason.nvim' },
-			{ 'williamboman/mason-lspconfig.nvim' },
-
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },
-			{ 'hrsh7th/cmp-buffer' },
-			{ 'hrsh7th/cmp-path' },
-			{ 'saadparwaiz1/cmp_luasnip' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/cmp-nvim-lua' },
-
-			-- Snippets
-			{ 'L3MON4D3/LuaSnip' },
-			{ 'rafamadriz/friendly-snippets' },
-		}
-	}
-end)
--- Install packages using :PackerSync
+	},
+	{ 'akinsho/bufferline.nvim',       version = "*", dependencies = 'nvim-tree/nvim-web-devicons' },
+	{ 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' } }
+})
 
 -- require plugins settings form this dir
-require('plugins.icons')
+-- require('plugins.icons')
 require('plugins.lualine')
 require('plugins.nvim-tree')
 require('plugins.bufferline')
-require('plugins.comment')
-require('plugins.nvim-treesitter')
-require('plugins.lsp-zero')
-require('plugins.gitsigns')
-require('plugins.toggleterm')
+-- require('plugins.comment')
+-- require('plugins.nvim-treesitter')
+-- require('plugins.lsp-zero')
+-- require('plugins.gitsigns')
+-- require('plugins.toggleterm')
