79fe3cffb49205d1b9901ccbf21427fdec6b3bf6
Manuel Rivera <menu.rivera@gmail.com>
2025-05-29 13:59:43 -0600

    fix: volar lsp

--- lua/plugins/config/lspconfig.lua
+++ lua/plugins/config/lspconfig.lua
@@ -44,7 +44,8 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
 	}
 )
 
-local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server'
+local vue_language_server_path = vim.fn.stdpath('data') ..
+	"/mason/packages/vue-language-server/node_modules/@vue/language-server"
 
 lspconfig.ts_ls.setup {
 	init_options = {
