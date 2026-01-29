14d1c3dad07b134c12f4932cef83aea41d5b9d38
Manuel Rivera <menu.rivera@gmail.com>
2026-01-28 19:40:56 -0600

    fix: cokeline shown always

--- lua/plugins/config/cokeline.lua
+++ lua/plugins/config/cokeline.lua
@@ -1,7 +1,7 @@
 local get_hex = require('cokeline.hlgroups').get_hl_attr
 
 require('cokeline').setup({
-	show_if_buffers_are_at_least = 2,
+	show_if_buffers_are_at_least = 1,
 	fill_hl = 'TabLineFill',
 	default_hl = {
 		fg = function(buffer)
@@ -26,7 +26,7 @@ require('cokeline').setup({
 
 				return buffer.is_focused
 					and " <" .. filename .. "> "
-					or "  " .. filename .. "  " 
+					or "  " .. filename .. "  "
 			end,
 
 
